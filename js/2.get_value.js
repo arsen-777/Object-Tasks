9f33ef8257df10b2c642dbc8e6e76f44579e1252
arsen-777 <arsengrigoryan3103@gmail.com>
2021-12-14 19:29:26 +0400

    Get values in object use destructuring

---
+++ js/2.get_value.js
@@ -0,0 +1,8 @@
+"use strict";
+
+let person = { name: "Sarah", country: "Armenia", job: "Developer" };
+
+const { name, country, job } = person;
+console.log(name); // "Sarah"
+console.log(country); // "Nigeria"
+console.log(job); // "Developer"
